64af7ec82639578f37d68eebda6ccec74ff5cae9
Masood Khan <masoodkhanse28@gmail.com>
2024-09-26 00:24:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
 
 st.set_page_config(
